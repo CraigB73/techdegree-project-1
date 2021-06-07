0aeddee8a6257b5c4c1483df4e7a5527f07d3759
Craig britton <craigbritton@Craigs-Air.lan>
2021-06-07 19:58:06 +0200

    updated info
    Added quotes and random function

--- RandomQuote/a_random_quote_generator-v1/js/script.js
+++ RandomQuote/a_random_quote_generator-v1/js/script.js
@@ -4,8 +4,8 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
@@ -29,5 +29,37 @@ project 1 - A Random Quote Generator
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
+
+let quotes = [
+  {
+    quote: 'The way to get started is to quit talking and begin doing',
+    sorce: 'Walt Disney',
+    citation: 'BrainTracy.com',
+    year: 2001,
+  },
+  {
+    quote: "Don't let yesterday take up too much of today.",
+    sorce: 'Will Rogers',
+    citation: 'BrainTracy.com',
+    year: 2002,
+  },
+  {
+    quote: 'Simplicity is teh ultimate sophistication',
+    sorce: 'Leonardo da Vinci',
+    citation: 'p3',
+    year: 2003,
+  },
+
+];
+// Random quote function
+
+function randomQuote() {
+  let oneQuote = Math.floor(Math.random() * quotes.length);
+  let quoteChoice = quotes[oneQuote];
+ 
+  let quoteMessage = `<h1><q>quoteChoice.quote</q></h1> + '-' + quoteChoice.sorce`;
+}
+
+
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
