40c531b7372b67de82fe8e30489661d3ade6939b
Craig britton <craigbritton@Craigs-Air.lan>
2021-06-15 17:04:40 +0200

    added function and condition
    added function to be able to load quotes to the HTML page.

--- RandomQuote/a_random_quote_generator-v1/js/script.js
+++ RandomQuote/a_random_quote_generator-v1/js/script.js
@@ -3,63 +3,68 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-// Check the "Project Resources" section of the project instructions
-// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-
-
-/***
- * `getRandomQuote` function
-***/
-
-
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
+/* Quote array of objects: quote, source; if available, citation and year*/
 let quotes = [
   {
     quote: 'The way to get started is to quit talking and begin doing',
-    sorce: 'Walt Disney',
-    citation: 'BrainTracy.com',
-    year: 2001,
+    source: 'Walt Disney',
+    citation: 'unknown',
+    year: 'unknown',
   },
   {
     quote: "Don't let yesterday take up too much of today.",
-    sorce: 'Will Rogers',
+    source: 'Will Rogers',
     citation: 'BrainTracy.com',
-    year: 2002,
+    year: 'unknown',
   },
   {
-    quote: 'Simplicity is teh ultimate sophistication',
-    sorce: 'Leonardo da Vinci',
-    citation: 'p3',
-    year: 2003,
-  },
+    quote: 'Simplicity is the ultimate sophistication',
+    source: 'Leonardo da Vinci',
+    citation: 'Quote Investigor.com',
+    year: 2015,
+  }
 
 ];
-// Random quote function
 
+/***
+ * Random quote function 
+ * assigns random number 0 - 2 to the quotes array index
+ * placed in a new variable 
+ * return new array to function
+ ***/
 function randomQuote() {
-  let oneQuote = Math.floor(Math.random() * quotes.length);
-  let quoteChoice = quotes[oneQuote];
- 
-  let quoteMessage = `<h1><q>quoteChoice.quote</q></h1> + '-' + quoteChoice.sorce`;
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  let quotesArray = quotes[randomNumber];
+  // console.log(quotesArray);
+  return quotesArray;
+
 }
 
+
+/***
+ *  PrintQuote function to display quote with each click of button
+ * assigned two vaiable: 1. that holds randomQuote function, 2. variable(html)that will hold what will be printed to the screen
+ * added conditions to check if there is a citation and year assigned to the quotes array.
+ * insert string to so it can be printed to the page.  
+ * return the html variable to the funcition to be used in the .addEventListener()
+ */
+function printQuote() {
+  let getRandomQuote = randomQuote();
+  let html = `
+  <p class= "quote">${getRandomQuote.quote}</p>
+  <p class= "source">${getRandomQuote.source}`;
+
+  if (getRandomQuote.citation !== 'unknown') {
+    html += `<span class= "citation">${getRandomQuote.citation}</span>`
+
+  };
+  if (getRandomQuote.year !== 'unknown') {
+    html += `<span class= "year">${getRandomQuote.year}</span></p>`;
+
+  };
+  document.getElementById('quote-box').innerHTML = html;
+  return (html)
+}
 
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
